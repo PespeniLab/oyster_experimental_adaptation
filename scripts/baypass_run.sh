4fb51cfc3f873e5e576d2eeeec9f31535f7cdf28
Reid S Brennan <rbrennan@pespenilab.uvm.edu>
2018-11-07 10:37:42 -0500

    new clust

--- scripts/baypass_run.sh
+++ scripts/baypass_run.sh
@@ -1,7 +1,9 @@
 
 #lets subset to only the control pops:
 
-cat ~/oyster/analysis/genobaypass | cut -f 1,2,5,6,9,10 -d " " > ~/oyster/analysis/baypass.33.geno.in
+cat ~/oyster/analysis/baypass.geno.in| cut -f 1,2,5,6,9,10 -d " " > ~/oyster/analysis/baypass.33.geno.in
+
+#cat ~/oyster/analysis/genobaypass | cut -f 1,2,5,6,9,10 -d " " > ~/oyster/analysis/baypass.33.geno.in
 
 #haploid pool size of each population
 echo "40 40 40 40 30 32" > ~/oyster/analysis/baypass.psize.in
@@ -18,7 +20,10 @@ cd ~/oyster/analysis/baypass
 ~/bin/baypass_2.1/sources/g_baypass -npop 3 -gfile ~/oyster/analysis/baypass.33.geno.in \
     -poolsizefile ~/oyster/analysis/baypass.33.psize.in  \
     -outprefix 33_core \
-    -nthreads 6 2>&1 | tee ~/oyster/log_out/baypass_33.stdout_$(date +"%F_%R").txt
+    -burnin 5000 \
+    -pilotlength 1500 \
+    -nval 2000 \
+    -nthreads 4 2>&1 | tee ~/oyster/log_out/baypass_33.stdout_$(date +"%F_%R").txt
 
 # 33 only, aux to identify salinity associations
 
@@ -28,22 +33,5 @@ cd ~/oyster/analysis/baypass
     -outprefix 33_aux \
     -omegafile 33_core_mat_omega.out \
     -auxmodel \
-    -nthreads 6 2>&1 | tee ~/oyster/log_out/baypass_33_aux.stdout_$(date +"%F_%R").txt
+    -nthreads 4 2>&1 | tee ~/oyster/log_out/baypass_33_aux.stdout_$(date +"%F_%R").txt
 
-# run all pops:
-
-~/bin/baypass_2.1/sources/g_baypass -npop 6 -gfile ~/oyster/analysis/baypass.geno.in \
-    -poolsizefile ~/oyster/analysis/baypass.psize.in  \
-    -outprefix all_core \
-    -nthreads 6 2>&1 | tee ~/oyster/log_out/baypass_all.stdout_$(date +"%F_%R").txt
-
-# all, aux to identify shifts in response to salinity
-
-~/bin/baypass_2.1/sources/g_baypass -npop 6 -gfile ~/oyster/analysis/baypass.geno.in \
-    -poolsizefile ~/oyster/analysis/baypass.psize.in  \
-    -efile ~/oyster/analysis/baypass.covariate.in \
-    -outprefix all_aux \
-    -omegafile all_core_mat_omega.out \
-    -scalecov \
-    -auxmodel \
-    -nthreads 6 2>&1 | tee ~/oyster/log_out/baypass_all_aux.stdout_$(date +"%F_%R").txt
